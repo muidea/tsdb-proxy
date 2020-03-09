5064f8224ba7a829850361170988ac955a11b1a0
muidea <rangh@foxmail.com>
2020-03-09 09:26:23 +0800

    update code

--- common/model/build.sh
+++ common/model/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 IMPORT_PATH=$GOPATH/src/github.com/gogo/protobuf/protobuf:$GOPATH/src:.
-protoc --proto_path=$IMPORT_PATH --gofast_out . *proto
+protoc --proto_path=$IMPORT_PATH --gofast_out=. *proto
